9e8a8cd4d975518ee8e284a509e53ebb9bc3feb3
Matthew Horwood <matt.horwood@mayden.co.uk>
2024-04-04 15:26:42 +0100

    Update the orbit script to just exit 0

--- whatAreYouDoing.app/Contents/Resources/Scripts/orbot.sh
+++ whatAreYouDoing.app/Contents/Resources/Scripts/orbot.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-. ~/bin/functions
+. ~/bin/enviroment
 
 POSTDATA="{\"userId\": \"\",\"taskId\": \"$1\",\"duration\": \"$2\",\"text\": \"$3\",\"date\": \"`date +"%Y/%m/%d"`\"}"
 
-curl -d "${POSTDATA}" \
--H "Content-Type: application/json" \
--H "user_agent: Slackbot, from laptop" \
--H "x-email-address: $4" \
-https://orbot.mayden.co.uk/timesheet
+# curl -d "${POSTDATA}" \
+# -H "Content-Type: application/json" \
+# -H "user_agent: Slackbot, from laptop" \
+# -H "x-email-address: $4" \
+# https://orbot.mayden.co.uk/timesheet
+exit 0
